d6eca5392f2a868a90360e2fc2a7fc9f7830977c
Lu05 <l.lober1989@gmail.com>
2022-05-09 20:38:45 +0200

    Backtester takes already opened trades into account

--- CoinLegsSignalBacktester/Backtest/Backtester.cs
+++ CoinLegsSignalBacktester/Backtest/Backtester.cs
@@ -23,8 +23,15 @@ namespace CoinLegsSignalBacktester.Backtest
             int lossCount = 0;
             int profitCount = 0;
             var btData = data.ToList();
+            var closedPositions = new Dictionary<string, List<Tuple<DateTime, DateTime>>>();
             foreach (var backtestData in btData)
             {
+                if (IsPositionAlreadyOpen(closedPositions, backtestData))
+                {
+                    ColorConsole.WriteWarning($"Position for {backtestData.Notification.MarketName} already open!");
+                    continue;
+                }
+
                 var result = strategy.Backtest(backtestData, strategyConfig);
 
                 if (result.State == BackTestResultState.Valid)
@@ -33,6 +40,7 @@ namespace CoinLegsSignalBacktester.Backtest
                     if (backtestData.Version > 1)
                     {
                         line += $" ===> duration {Math.Round(result.Duration.TotalMinutes, 0)} min";
+                        AddToClosedPositions(backtestData, closedPositions, result.Duration);
                     }
                     if (result.PnL > 0)
                     {
@@ -54,7 +62,7 @@ namespace CoinLegsSignalBacktester.Backtest
                 {
                     ColorConsole.WriteWarning($"{backtestData.Notification.MarketName} ===> invalid!!!");
                 }
-
+                
                 if (plot is true)
                 {
                     _plotter.Plot(strategy, backtestData, result.State);
@@ -68,6 +76,53 @@ namespace CoinLegsSignalBacktester.Backtest
             var days = (dates.Last().Date - dates.First().Date).TotalDays;
             ColorConsole.WriteInfo($"days {Math.Round(days, 2)}");
             ColorConsole.WriteInfo($"avg daily profit {Math.Round((profit/(decimal)days) * 100, 3)}%");
+        }
+
+        /// <summary>
+        /// Adds the backtest result to the closed positions
+        /// </summary>
+        /// <param name="backtestData">data to add</param>
+        /// <param name="closedPositions">list of all closed positions</param>
+        /// <param name="duration">duration of the backtest</param>
+        private static void AddToClosedPositions(BacktestData backtestData, Dictionary<string, List<Tuple<DateTime, DateTime>>> closedPositions, TimeSpan duration)
+        {
+            var openClosed = new Tuple<DateTime, DateTime>(backtestData.Date, backtestData.Date.Add(duration));
+            if (closedPositions.TryGetValue(backtestData.Notification.MarketName, out List<Tuple<DateTime, DateTime>> positions))
+            {
+                positions.Add(openClosed);
+            }
+            else
+            {
+                closedPositions.Add(backtestData.Notification.MarketName, new List<Tuple<DateTime, DateTime>>
+                {
+                    openClosed
+                });
+            }
+        }
+
+        /// <summary>
+        /// Checks if a position is already open for the same symbol
+        /// </summary>
+        /// <param name="closedPositions">all closed positions</param>
+        /// <param name="data">current backtest data</param>
+        /// <returns>true, if the position is already open, otherwise false</returns>
+        private bool IsPositionAlreadyOpen(Dictionary<string, List<Tuple<DateTime, DateTime>>> closedPositions, BacktestData data)
+        {
+            if (!closedPositions.TryGetValue(data.Notification.MarketName, out List<Tuple<DateTime, DateTime>> positions))
+            {
+                return false;
+            }
+
+            for (int i = positions.Count - 1; i >= 0; i--)
+            {
+                var current = positions[i];
+                if (data.Date > current.Item1 && data.Date < current.Item2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
